973a52e439172aee66d39ecfd952d39c5b0f8f22
Lucas_Williams <101234675+LucasWilliams2622@users.noreply.github.com>
2023-05-25 00:43:34 +0700

    init project

---
+++ app.js
@@ -0,0 +1,65 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+// session,cookies
+const session=require('express-session');
+const mongoose=require('mongoose');
+// import cac model
+
+
+
+
+
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+
+
+// khai bao thong tin cua session
+app.use(session({
+  secret: 'iloveyou',//bi mat.
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+mongoose.connect('mongodb://127.0.0.1:27017/CP17310_2?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => console.log('>>>>>>>>>> DB Connected!!!!!!'))
+  .catch(err => console.log('>>>>>>>>> DB Error: ', err));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
